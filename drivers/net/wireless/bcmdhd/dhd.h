9929f65e82614e977861511c048f677f1d3e22bd
Leo He <leoh@nvidia.com>
2016-03-04 02:57:30 +0000

    net: wireless: bcmdhd: reduce packet timeout to 100ms
    bug 200153029
    
    This packet timeout prevents system entering syspend.
    This 500ms timeout is too long, which will slow
    resume/suspend cycle in WiFi wakeup, so reducing it to 100ms.
    Also, BRCM confirmed that it's safe to remove this timeout.
    
    Change-Id: I62dd11e292d3c9fbfe959d76fc510c2115958b7e
    Signed-off-by: Leo He <leoh@nvidia.com>
    Reviewed-on: http://git-master/r/1023810
    (cherry picked from commit e1395c20bf28b4eb5412534d46da9949db6cc8a0)
    Reviewed-on: https://git-master.nvidia.com/r/1785736
    Reviewed-by: Om Prakash Singh <omp@nvidia.com>
    Tested-by: Om Prakash Singh <omp@nvidia.com>
    Reviewed-by: Nagaraj Annaiah <nannaiah@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Ashutosh Jha <ajha@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/net/wireless/bcmdhd/dhd.h
+++ drivers/net/wireless/bcmdhd/dhd.h
@@ -544,7 +544,7 @@ inline static void MUTEX_UNLOCK_SOFTAP_SET(dhd_pub_t * dhdp)
 #define DHD_OS_OOB_IRQ_WAKE_LOCK_TIMEOUT(pub, val) dhd_os_oob_irq_wake_lock_timeout(pub, val)
 #define DHD_OS_OOB_IRQ_WAKE_UNLOCK(pub)		dhd_os_oob_irq_wake_unlock(pub)
 #endif /* BCMPCIE_OOB_HOST_WAKE */
-#define DHD_PACKET_TIMEOUT_MS	500
+#define DHD_PACKET_TIMEOUT_MS	100
 #define DHD_EVENT_TIMEOUT_MS	1500
 
 
