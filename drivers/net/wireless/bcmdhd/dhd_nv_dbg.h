InES-HPMM/linux-l4t

bcmdhd driver: remove unwanted error prints.
Add NV specific print with timestamp

Bug 1729783

Change-Id: I8dd598cd065d5c5be2217d25279ed9c7f83b8eef
Signed-off-by: Mahesh Patil <maheshp@nvidia.com>
Reviewed-on: http://git-master/r/1020976
GVS: Gerrit_Virtual_Submit
Reviewed-by: Ashutosh Jha <ajha@nvidia.com>

drivers/net/wireless/bcmdhd/dhd_nv_dbg.h
@@ -1,0 +1,47 @@
+/*
+ * drivers/net/wireless/bcmdhd/dhd_nv_dbg.h
+ *
+ * NVIDIA Tegra Debug prints for BCMDHD driver
+ *
+ * Copyright (c) 2016, NVIDIA CORPORATION. All rights reserved.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef _dhd_nv_dbg_h_
+#define _dhd_nv_dbg_h_
+
+#include <linux/time.h>
+
+#define NV_TIMESTAMP()		\
+				do {	\
+					struct timeval now;	\
+					struct tm date_time;	\
+					do_gettimeofday(&now);		\
+					time_to_tm(now.tv_sec, -sys_tz.tz_minuteswest * 60, &date_time);	\
+					pr_info("[%.2d-%.2d %.2d:%.2d:%.2d.%u] %s: ",	\
+						date_time.tm_mon+1, date_time.tm_mday, date_time.tm_hour,	\
+						date_time.tm_min,date_time.tm_sec ,	\
+						(unsigned int)(now.tv_usec/1000), __func__);	\
+				} while(0)
+
+#define DHD_NV_PRINT(args) 	\
+								do {	\
+									NV_TIMESTAMP();\
+									pr_cont args;	\
+								} while(0)
+
+#define DHD_NV_INFO(args)	do {	DHD_NV_PRINT(args);	} while(0)
+#define DHD_NV_DEBUG(args)	do {	DHD_NV_PRINT(args);	} while(0)
+#define DHD_NV_ERROR(args)	do {	DHD_NV_PRINT(args);	} while(0)
+
+#endif  /* _dhd_nv_dbg_h_ */
+

CTCaer/switch-l4t-kernel-4.9

(cherry picked from commit 8df1a0e157e3602ad7e09ff87d88502f767c9c8b)
Reviewed-on: https://git-master.nvidia.com/r/1785268
Reviewed-by: Om Prakash Singh <omp@nvidia.com>
Tested-by: Om Prakash Singh <omp@nvidia.com>
Reviewed-by: Shobek Attupurath <sattupurath@nvidia.com>
Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

drivers/net/wireless/bcmdhd/dhd_nv_dbg.h
@@ -1,0 +1,47 @@
+/*
+ * drivers/net/wireless/bcmdhd/dhd_nv_dbg.h
+ *
+ * NVIDIA Tegra Debug prints for BCMDHD driver
+ *
+ * Copyright (c) 2016, NVIDIA CORPORATION. All rights reserved.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef _dhd_nv_dbg_h_
+#define _dhd_nv_dbg_h_
+
+#include <linux/time.h>
+
+#define NV_TIMESTAMP()		\
+				do {	\
+					struct timeval now;	\
+					struct tm date_time;	\
+					do_gettimeofday(&now);		\
+					time_to_tm(now.tv_sec, -sys_tz.tz_minuteswest * 60, &date_time);	\
+					pr_info("[%.2d-%.2d %.2d:%.2d:%.2d.%u] %s: ",	\
+						date_time.tm_mon+1, date_time.tm_mday, date_time.tm_hour,	\
+						date_time.tm_min, date_time.tm_sec,	\
+						(unsigned int)(now.tv_usec/1000), __func__);	\
+				} while (0)
+
+#define DHD_NV_PRINT(args) 	\
+								do {	\
+									NV_TIMESTAMP();\
+									pr_cont args;	\
+								} while (0)
+
+#define DHD_NV_INFO(args)	do {	DHD_NV_PRINT(args);	} while (0)
+#define DHD_NV_DEBUG(args)	do {	DHD_NV_PRINT(args);	} while (0)
+#define DHD_NV_ERROR(args)	do {	DHD_NV_PRINT(args);	} while (0)
+
+#endif  /* _dhd_nv_dbg_h_ */
+