9fa9643d9221db5f8e2827970110cbf4f318438b
Billy Laws <blaws05@gmail.com>
2019-03-15 22:10:22 +0000

    dont powergate usb

--- drivers/platform/tegra/powergate/powergate-t21x.c
+++ drivers/platform/tegra/powergate/powergate-t21x.c
@@ -1392,9 +1392,9 @@ static int tegra210_pg_init_refcount(void)
 	t210_pg_info[TEGRA210_POWER_DOMAIN_NVJPG].part_info->refcount +=
 		(tegra210_pg_is_powered(TEGRA210_POWER_DOMAIN_NVDEC) ? 1 : 0);
 
-	tegra210_pg_powergate_partition(TEGRA210_POWER_DOMAIN_XUSBA);
-	tegra210_pg_powergate_partition(TEGRA210_POWER_DOMAIN_XUSBB);
-	tegra210_pg_powergate_partition(TEGRA210_POWER_DOMAIN_XUSBC);
+	//tegra210_pg_powergate_partition(TEGRA210_POWER_DOMAIN_XUSBA);
+	//tegra210_pg_powergate_partition(TEGRA210_POWER_DOMAIN_XUSBB);
+	//tegra210_pg_powergate_partition(TEGRA210_POWER_DOMAIN_XUSBC);
 
 	return 0;
 }
