51cb8e327dcf860ded010b5ef138df1779517590
langerhans <langerhans@users.noreply.github.com>
2020-01-02 14:16:44 +0100

    Revert "powergate: t210: don't powergate usb on switch"
    This reverts commit fe68186364bd4761bb64f6d915153216aee4ce1e.

--- drivers/platform/tegra/powergate/powergate-t21x.c
+++ drivers/platform/tegra/powergate/powergate-t21x.c
@@ -33,7 +33,6 @@
 #include <soc/tegra/fuse.h>
 #include <linux/platform/tegra/mc.h>
 #include <soc/tegra/pmc.h>
-#include <linux/of_platform.h>
 
 #define MAX_CLK_EN_NUM			15
 #define MAX_HOTRESET_CLIENT_NUM		4
@@ -1393,11 +1392,9 @@ static int tegra210_pg_init_refcount(void)
 	t210_pg_info[TEGRA210_POWER_DOMAIN_NVJPG].part_info->refcount +=
 		(tegra210_pg_is_powered(TEGRA210_POWER_DOMAIN_NVDEC) ? 1 : 0);
 
-	if (!of_machine_is_compatible("nintendo,icosa")) {
-		tegra210_pg_powergate_partition(TEGRA210_POWER_DOMAIN_XUSBA);
-		tegra210_pg_powergate_partition(TEGRA210_POWER_DOMAIN_XUSBB);
-		tegra210_pg_powergate_partition(TEGRA210_POWER_DOMAIN_XUSBC);
-	}
+	tegra210_pg_powergate_partition(TEGRA210_POWER_DOMAIN_XUSBA);
+	tegra210_pg_powergate_partition(TEGRA210_POWER_DOMAIN_XUSBB);
+	tegra210_pg_powergate_partition(TEGRA210_POWER_DOMAIN_XUSBC);
 
 	return 0;
 }
