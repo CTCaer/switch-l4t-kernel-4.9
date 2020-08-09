bdda496b77bdb860b8cf4b42fabcf83b3b5fec8b
CTCaer <ctcaer@gmail.com>
2020-08-09 09:28:11 +0000

    bt: bcm: Remove forced debug log

--- drivers/bluetooth/hci_bcm.c
+++ drivers/bluetooth/hci_bcm.c
@@ -20,7 +20,7 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  */
-#define DEBUG 1
+
 #include <linux/kernel.h>
 #include <linux/errno.h>
 #include <linux/skbuff.h>
