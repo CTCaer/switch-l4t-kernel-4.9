e3109d67a628c3a13986fd6b907ef0552be0507d
CTCaer <ctcaer@gmail.com>
2020-09-29 20:20:57 +0100

    iio: core: Remove unnecessary sysfs linking
    This causes an error when multiple iio devices are reported by a single
    driver, such is the case with lsm6dsx.

--- drivers/iio/industrialio-core.c
+++ drivers/iio/industrialio-core.c
@@ -1594,19 +1594,8 @@ int iio_device_register(struct iio_dev *indio_dev)
 	if (ret < 0)
 		goto error_cdev_del;
 
-	ret = sysfs_create_link(&indio_dev->dev.parent->kobj,
-				&indio_dev->dev.kobj,
-				indio_dev->dev_type.name);
-	if (ret) {
-		dev_err(indio_dev->dev.parent,
-			"Failed to create link for iio_device %d\n", ret);
-		goto error_del_device;
-	}
-
 	return 0;
 
-error_del_device:
-	device_del(&indio_dev->dev);
 error_cdev_del:
 	cdev_del(&indio_dev->chrdev);
 error_unreg_eventset:
