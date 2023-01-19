9673a5ba1cac68a28e9bd7e0a9c51f960085d784
CTCaer <ctcaer@gmail.com>
2023-01-19 16:38:39 +0200

    brcmfmac: fix copyrights

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/common.h
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/common.h
@@ -1,4 +1,5 @@
 /* Copyright (c) 2014 Broadcom Corporation
+ * Copyright (c) 2020, CTCaer.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
