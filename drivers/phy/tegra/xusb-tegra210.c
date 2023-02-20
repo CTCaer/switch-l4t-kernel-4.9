c90392b6b24c236d7ae1092f327f9d580ed7acba
CTCaer <ctcaer@gmail.com>
2023-02-20 20:36:17 +0200

    phy: tegra210: xusb: update copyright

--- drivers/phy/tegra/xusb-tegra210.c
+++ drivers/phy/tegra/xusb-tegra210.c
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2014-2019, NVIDIA CORPORATION.  All rights reserved.
  * Copyright (C) 2015 Google, Inc.
- * Copyright (C) 2022 CTCaer
+ * Copyright (C) 2022-2023 CTCaer
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms and conditions of the GNU General Public License,
