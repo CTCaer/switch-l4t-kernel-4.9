e803b6e81ed7d901c22b07445eec653d00b6d2d4
CTCaer <ctcaer@gmail.com>
2023-02-20 20:36:31 +0200

    pci: host: tegra: update copyright

--- drivers/pci/host/pci-tegra.c
+++ drivers/pci/host/pci-tegra.c
@@ -9,6 +9,8 @@
  *
  * Bits taken from arch/arm/mach-dove/pcie.c
  *
+ * Copyright (c) 2023, CTCaer.
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
